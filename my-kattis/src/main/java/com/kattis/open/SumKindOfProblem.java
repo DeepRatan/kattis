d4e85a809703bf2e3d00b98ca6949a8d91ae07ba
Deep Ratan <deepratans@gmail.com>
2019-06-05 22:52:47 +0800

    Summing numbers

--- my-kattis/src/main/java/com/kattis/open/SumKindOfProblem.java
+++ my-kattis/src/main/java/com/kattis/open/SumKindOfProblem.java
@@ -1,24 +1,30 @@
 package com.kattis.open;
-
+/**
+ * kattis.com
+ * Problem: Summing numbers
+ * Solution attempt by Deep Ratan (deepratans@gmail.com)
+ * Date: 05-June-2019
+ */
+import java.util.Scanner;
 public class SumKindOfProblem {
-
 	public static void main(String[] args) {
-		int iterations = Integer.parseInt(args[0]);
-		for(int i=1;i<=iterations;i++){
-			int odd = 1;
-			int even = 2;
-			int s1=0;
-			int s2=0;
-			int s3=0;
-			String[] stringArray = args[i].split(" ");
-			int numberOfLoops = Integer.parseInt(stringArray[1]);
-			for(int j=1; j<=numberOfLoops;j++){
-				s1+=j;
-				s2+=odd;
-				s3+=even;
-				odd+=2; even+=2;
-			}//end of first internal for loop
-			System.out.println(stringArray[0]+" "+ s1 + " " + s2 + " " + s3 );
-		}//end of outer for loop
+		Scanner scanner = new Scanner(System.in);
+		int noOfInputs = Integer.parseInt(scanner.nextLine());
+		String[] inputs = new String[noOfInputs];
+		for(int i=0;i<noOfInputs;i++){
+			inputs[i] = scanner.nextLine();			
+		}		
+		for(String s: inputs){
+			int s1 =0, s2=0, s3=0;
+			String[] strAry = s.split("\\W+");
+			int length = Integer.parseInt(strAry[1]);
+			for(int i=1,j=2,k=1;i<=length;i++,j+=2,k+=2){
+				s1+=i;
+				s2+=k;
+				s3+=j;
+			}			
+			System.out.println(strAry[0]+" "+s1+" "+s2+" "+s3);
+		}
+		scanner.close();
 	}
 }
