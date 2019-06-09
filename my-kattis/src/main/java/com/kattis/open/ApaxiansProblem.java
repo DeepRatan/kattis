b4f1fc873a4cbd2c4af58820abbe037869f5f06c
Deep Ratan <deepratans@gmail.com>
2019-06-09 14:29:58 +0800

    Modified code

--- my-kattis/src/main/java/com/kattis/open/ApaxiansProblem.java
+++ my-kattis/src/main/java/com/kattis/open/ApaxiansProblem.java
@@ -6,10 +6,9 @@ public class ApaxiansProblem {
 		String inputName = scanner.nextLine();
 		int length = inputName.length();
 		StringBuilder strBuilder = new StringBuilder();
-		for(int i=0;i<length;i++){
-			if(i==0){
-				strBuilder.append(inputName.charAt(i));
-			}else if(!(strBuilder.charAt(strBuilder.length()-1)==inputName.charAt(i))){
+		strBuilder.append(inputName.charAt(0));
+		for(int i=1;i<length;i++){
+			if(!(strBuilder.charAt(strBuilder.length()-1)==inputName.charAt(i))){
 				strBuilder.append(inputName.charAt(i));
 			}
 		}
