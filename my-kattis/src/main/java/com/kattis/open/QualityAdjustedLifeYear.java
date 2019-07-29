2aa8fca6464128f2a386eef9ae3ae23d60b23d99
Deep Ratan <deepratans@gmail.com>
2019-07-29 10:31:10 +0800

    QALY code

---
+++ my-kattis/src/main/java/com/kattis/open/QualityAdjustedLifeYear.java
@@ -0,0 +1,16 @@
+package com.kattis.open;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+public class QualityAdjustedLifeYear {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double qaly = 0;
+		int period = sc.nextInt();
+		for(int i=0;i<period;i++){
+			double q=sc.nextDouble(), p=sc.nextDouble();
+			qaly+=(q*p);
+		}
+		System.out.printf("%.3f",qaly);
+		sc.close();			
+	}
+}
