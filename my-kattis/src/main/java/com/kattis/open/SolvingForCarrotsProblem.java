4fc73d2abdea2671056a39aa483e8916fd0182b1
Deep Ratan <deepratans@gmail.com>
2020-10-26 18:24:48 +0800

    Carrot Problem

---
+++ my-kattis/src/main/java/com/kattis/open/SolvingForCarrotsProblem.java
@@ -0,0 +1,14 @@
+package com.kattis.open;
+
+import java.util.Scanner;
+
+public class SolvingForCarrotsProblem {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int numOfCandidates = sc.nextInt();
+		int numOfProblemsSolved = sc.nextInt();
+		
+		System.out.println(numOfProblemsSolved);
+		sc.close();
+	}
+}
